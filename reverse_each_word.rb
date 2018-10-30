8d39e74c8092b0d6538a7d94f09e096d083e5340
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 01:41:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  reversed_sentence = sentence.split do |sentence|
+  reversed_sentence = sentence.split("") do |sentence|
     puts sentence.reverse 
   end 
 end   

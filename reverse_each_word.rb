9a26095c51de2f06fa6cc300eaa6237f57985801
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 01:47:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,25 +4,25 @@ def reverse_each_word(array)
   end 
 end   
 
-def reverse_each_word(array)
-  new_array = array.split(" ")
-  reversed_array = new_array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
-end
+# def reverse_each_word(array)
+#   new_array = array.split(" ")
+#   reversed_array = new_array.each {|x| x.reverse!}
+#   return reversed_array.join(" ")
+# end
 
-def reverse_each_word(sentence)
-  sentence.collect do |sentence|
-    puts sentence.reverse 
-  end 
-end   
+# def reverse_each_word(sentence)
+#   sentence.collect do |sentence|
+#     puts sentence.reverse 
+#   end 
+# end   
 
-def reverse_each_word(array)
-  new_array = array.split(" ")
-  new_array.collect {|x| x.reverse!}
-  new_array.join(" ")
-end
+# def reverse_each_word(array)
+#   new_array = array.split(" ")
+#   new_array.collect {|x| x.reverse!}
+#   new_array.join(" ")
+# end
 
-# FINAL SOLUTION
-def reverse_each_word(array)
-  array.split(" ").collect {|x| x.reverse!}.join(" ")
-end
+# # FINAL SOLUTION
+# def reverse_each_word(array)
+#   array.split(" ").collect {|x| x.reverse!}.join(" ")
+# end

8ce7a8e3a4ac44865e7b46803bda1eb6c6da1c46
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 02:13:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(array)
     array.split(" ").reverse
     new_array = []
-    reversed_array.each {|array| array.reverse}
+    new_array.each {|array| array.reverse}
   return 
   array.each.join("")
 end   

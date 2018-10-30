e6be41aad23aaa1ce926c3e704af71c8f9899ecb
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 01:40:06 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  sentence = array.split do |sentence|
+  reversed_sentence = sentence.split do |sentence|
     puts sentence.reverse 
   end 
 end   
@@ -9,3 +9,14 @@ def reverse_each_word(sentence)
     puts sentence.reverse 
   end 
 end   
+
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
+end
+
+# FINAL SOLUTION
+def reverse_each_word(array)
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
+end

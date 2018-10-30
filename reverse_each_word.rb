0def8911eb7de8c784b3d2b72f42bc80893e664f
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 01:36:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,9 @@ def reverse_each_word(sentence)
     puts sentence.reverse 
   end 
 end   
+
+def reverse_each_word(sentence)
+  sentence.each do |sentence|
+    puts sentence.reverse 
+  end 
+end   

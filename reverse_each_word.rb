bed4752097f40a998229f940770dd63d2799b3c9
tatiana.mojica <tatiana.mojica@bustle.com>
2018-10-30 02:41:32 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
  
-def reverse_each_word(sentence)
+def reverse_each_word(array)
   new_array = array.split(" ")
   reversed_array = new_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
